5153daf2b0ae9e8e6e7e2dc6abade5b7adb56f35
tinycodecraft <raymond.cw.yau@gmail.com>
2024-01-16 17:10:00 +0800

    try do some update on library in vite

--- vteCore/ClientApp/vite.config.ts
+++ vteCore/ClientApp/vite.config.ts
@@ -1,9 +1,15 @@
 import serverOption from './serverOption'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath,URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server : serverOption,
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },  
 })
