d747ecfbfede8e34ffee75c0818b9353fb5d90e0
tinycodecraft <raymond.cw.yau@gmail.com>
2024-02-14 14:45:59 +0800

    add bfa context project

---
+++ vteCore.dbBFA/Partials/Factories.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace vteCore.dbBFA.Models
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BFAContext>
+    {
+        IHostEnvironment _env;
+
+        public DesignTimeDbContextFactory()
+        {
+
+        }
+
+        public DesignTimeDbContextFactory(IHostEnvironment env)
+        {
+            _env = env;
+
+        }
+        public BFAContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(@Directory.GetCurrentDirectory() + "/../vteCore/appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<BFAContext>();
+            var connectionString = configuration.GetConnectionString("dbBFA");
+            builder.UseSqlServer(connectionString, opt => opt.MigrationsAssembly("vteCore.dbBFA"));
+            return new BFAContext(builder.Options, _env);
+
+        }
+    }
+}
---
+++ vteCore.dbBFA/Partials/Partials.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vteCore.dbBFA.Models;
+
+
+public partial class BFAContext
+{
+    IHostEnvironment _env;
+    public BFAContext(DbContextOptions<BFAContext> options, IHostEnvironment env)
+        : base(options)
+    {
+        _env = env;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+
+        base.OnConfiguring(optionsBuilder);
+        IConfigurationRoot configuration = null;
+
+        if (!_env.IsProduction())
+        {
+            configuration = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile(@Directory.GetCurrentDirectory() + "/../vteCore/appsettings.json")
+                                .Build();
+        }
+        else
+        {
+            configuration = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile(@Directory.GetCurrentDirectory() + "/appsettings.json", optional: false, reloadOnChange: true)
+                                .AddJsonFile(@Directory.GetCurrentDirectory() + $"/appsettings.{_env.EnvironmentName}.json", optional: true)
+                                .Build();
+        }
+
+        var builder = new DbContextOptionsBuilder<BFAContext>();
+        var connectionString = configuration.GetConnectionString("dbBFA");
+        optionsBuilder.UseSqlServer(connectionString);
+
+    }
+}
