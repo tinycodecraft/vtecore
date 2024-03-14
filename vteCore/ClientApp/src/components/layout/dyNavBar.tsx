769d14351e05c9c99d1cfacfed7f6bc4a4c38783
tinycodecraft <raymond.cw.yau@gmail.com>
2024-03-14 10:22:07 +0800

    add hamburger

--- vteCore/ClientApp/src/components/layout/dyNavBar.tsx
+++ vteCore/ClientApp/src/components/layout/dyNavBar.tsx
@@ -41,13 +41,17 @@ import routes from '@/constants/routes/config'
 import { HubInit, HubState, MenuPositionEnum } from '@/constants/types'
 import { useAppSelector, useWindowSize } from '@/hooks'
 import { clsxm } from '@/utils/methods'
+import { DyHamburger } from '../dyHamburger'
 
 export const DyNavBar = () => {
   const { classes: menuLinkStyle, theme } = useMenuLinkStyle()
   const [openHovered, setOpenHover] = useState(false)
   const windowSize = useWindowSize()
   const [navRef, navRect] = useResizeObserver()
-  const [isfullwidth, setfullwidth] = useState<boolean>(!windowSize.winWidth || windowSize.winWidth > navRect.width)
+  const [midRef, midRect] = useResizeObserver()
+  const [isfullwidth, setfullwidth] = useState<boolean>(
+    !midRect || !navRect || midRect.width > navRect.width + 50 || navRect.width < 150,
+  )
   const [isNavOpen, setNavOpen] = useState<boolean>(false)
   const { token, controlAdminEnabled, userName } = useAppSelector<HubState>((state) => state.dmHub ?? HubInit)
   const icons = [IconHome, IconLock, IconCloudFog, IconLogout, IconSwitch3, IconList]
@@ -56,9 +60,9 @@ export const DyNavBar = () => {
   useEffect(() => {
     if (windowSize.winWidth) {
       console.log(`the center menu width `, navRect.width)
-      setfullwidth(windowSize.winWidth > navRect.width)
+      setfullwidth(midRect.width > navRect.width + 50 || navRect.width < 150)
     }
-  }, [windowSize, navRect])
+  }, [windowSize, navRect, midRect])
 
   const mockdata = [
     {
@@ -96,78 +100,83 @@ export const DyNavBar = () => {
   return (
     <div className="navbar bg-base-100" data-theme="nord">
       <div className="flex-none">
-        <a className="btn btn-ghost text-2xl font-blck">Ray Studio</a>
+        <a className="btn btn-ghost text-2xl font-blck">
+          Ray Studio
+          {!isfullwidth && <DyHamburger />}
+        </a>
       </div>
-      <div className="flex-grow justify-center" ref={navRef}>
-        <ul className="menu menu-horizontal px-1">
-          {routes
-            .filter((e) => e.position === MenuPositionEnum.center && (!e.locked || token))
-            .map((route, index) => {
-              return (
-                <li
-                  className={clsxm(`${menuLinkStyle.simplelink}`, 'underline-flash', 'font-quan', 'text-lg')}
-                  key={`${route.name}-${index}`}
-                >
-                  <NavLink key={route.name} to={generatePath(route.path, route.params)}>
-                    {React.createElement(icons[route.iconIndex ?? 0], { className: 'h-[18px] w-[18px] inline' })}
-                    {route.name}
-                  </NavLink>
-                </li>
-              )
-            })}
+      <div className="flex-grow justify-center" ref={midRef}>
+        {isfullwidth && (
+          <ul className="menu menu-horizontal px-1" ref={navRef}>
+            {routes
+              .filter((e) => e.position === MenuPositionEnum.center && (!e.locked || token))
+              .map((route, index) => {
+                return (
+                  <li
+                    className={clsxm(`${menuLinkStyle.simplelink}`, 'underline-flash', 'font-quan', 'text-lg')}
+                    key={`${route.name}-${index}`}
+                  >
+                    <NavLink key={route.name} to={generatePath(route.path, route.params)}>
+                      {React.createElement(icons[route.iconIndex ?? 0], { className: 'h-[18px] w-[18px] inline' })}
+                      {route.name}
+                    </NavLink>
+                  </li>
+                )
+              })}
 
-          <li className="underline-flash">
-            <HoverCard
-              width={600}
-              position="bottom"
-              radius={'md'}
-              shadow="md"
-              withinPortal
-              // setstate problem
-              // onOpen={openclose}
-              // onClose={openclose}
-            >
-              <HoverCard.Target>
-                <a className={clsxm(menuLinkStyle.menulink, 'font-quan', 'text-lg')}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      Features
-                    </Box>
-                    <IconChevronDown size={16} color={theme.fn.primaryColor()} className="arrow" />
-                  </Center>
-                </a>
-              </HoverCard.Target>
-              <HoverCard.Dropdown sx={{ overflow: 'hidden' }}>
-                <Group position="apart" px="md">
-                  <Text fw={500}>Features</Text>
-                  <Anchor href="#" fz="xs">
-                    View all
-                  </Anchor>
-                </Group>
-                <Divider my="sm" mx="-md" color={theme.colorScheme === 'dark' ? 'brand.5' : 'brand.1'} />
-                <SimpleGrid cols={2} spacing={0}>
-                  {mockdata.map((item, index) => (
-                    <UnstyledButton className={menuLinkStyle.childLink} key={`${item.title}-${index}`}>
-                      <Group noWrap align="flex-start">
-                        <ThemeIcon size={34} variant="default" radius="md">
-                          <item.icon size={rem(22)} color={theme.fn.primaryColor()} />
-                        </ThemeIcon>
-                        <div>
-                          <Text size="sm" fw={500}>
-                            {item.title}
-                          </Text>
-                          <Text size="xs" color="yellow.0" className="font-gabr">
-                            {item.description}
-                          </Text>
-                        </div>
-                      </Group>
-                    </UnstyledButton>
-                  ))}
-                </SimpleGrid>
-              </HoverCard.Dropdown>
-            </HoverCard>
-          </li>
-        </ul>
+            <li className="underline-flash">
+              <HoverCard
+                width={600}
+                position="bottom"
+                radius={'md'}
+                shadow="md"
+                withinPortal
+                // setstate problem
+                // onOpen={openclose}
+                // onClose={openclose}
+              >
+                <HoverCard.Target>
+                  <a className={clsxm(menuLinkStyle.menulink, 'font-quan', 'text-lg')}>
+                    <Center inline>
+                      <Box component="span" mr={5}>
+                        Features
+                      </Box>
+                      <IconChevronDown size={16} color={theme.fn.primaryColor()} className="arrow" />
+                    </Center>
+                  </a>
+                </HoverCard.Target>
+                <HoverCard.Dropdown sx={{ overflow: 'hidden' }}>
+                  <Group position="apart" px="md">
+                    <Text fw={500}>Features</Text>
+                    <Anchor href="#" fz="xs">
+                      View all
+                    </Anchor>
+                  </Group>
+                  <Divider my="sm" mx="-md" color={theme.colorScheme === 'dark' ? 'brand.5' : 'brand.1'} />
+                  <SimpleGrid cols={2} spacing={0}>
+                    {mockdata.map((item, index) => (
+                      <UnstyledButton className={menuLinkStyle.childLink} key={`${item.title}-${index}`}>
+                        <Group noWrap align="flex-start">
+                          <ThemeIcon size={34} variant="default" radius="md">
+                            <item.icon size={rem(22)} color={theme.fn.primaryColor()} />
+                          </ThemeIcon>
+                          <div>
+                            <Text size="sm" fw={500}>
+                              {item.title}
+                            </Text>
+                            <Text size="xs" color="yellow.0" className="font-gabr">
+                              {item.description}
+                            </Text>
+                          </div>
+                        </Group>
+                      </UnstyledButton>
+                    ))}
+                  </SimpleGrid>
+                </HoverCard.Dropdown>
+              </HoverCard>
+            </li>
+          </ul>
+        )}
       </div>
       <div className="flex-none">
         <LanguageControl className="mr-2" />
