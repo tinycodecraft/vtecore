2592113798146c6b47096ac75648d2b83240f902
tinycodecraft <raymond.cw.yau@gmail.com>
2024-04-11 10:12:40 +0800

    fix the aside menu fade out transition

--- vteCore/ClientApp/src/components/layout/LeftDrawer.tsx
+++ vteCore/ClientApp/src/components/layout/LeftDrawer.tsx
@@ -13,10 +13,10 @@ export const LeftDrawer = ({ children, title }: PropsWithChildren<LeftDrawerProp
   return (
     <main
       className={
-        ' fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ' +
+        ' fixed overflow-hidden z-10  inset-0 transform bg-purple-300 bg-opacity-25 ease-in-out ' +
         (isNavOpen
-          ? ' transition-opacity opacity-100 duration-500 translate-x-0  '
-          : ' transition-all delay-500 opacity-0 -translate-x-full  ')
+          ? ' transition-opacity opacity-100 duration-800 translate-x-0  '
+          : ' transition-all delay-800 opacity-0 -translate-x-full  ')
       }
       data-theme="nord"
     >
