a3e6711af03d8eccacd13cd580302bd4ad7f06fb
tinycodecraft <raymond.cw.yau@gmail.com>
2024-04-19 15:29:17 +0800

    try to add environment for windowservice

---
+++ vteCore.jbKeeper/JobKeeper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vteCore.jbKeeper
+{
+    public sealed class JobKeeper: BackgroundService
+    {
+        private readonly ILogger<JobKeeper> log;
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    log.LogInformation("the job keeper is still alive!");
+
+                    await Task.Delay(TimeSpan.FromSeconds(10),stoppingToken);
+                }
+
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+            catch(Exception ex)
+            {
+                log.LogError(ex, ex.Message);
+                // Terminates this process and returns an exit code to the operating system.
+                // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+                // performs one of two scenarios:
+                // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+                // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+                //
+                // In order for the Windows Service Management system to leverage configured
+                // recovery options, we need to terminate the process with a non-zero exit code.
+                Environment.Exit(1);
+            }
+        }
+    }
+}
--- vteCore.jbKeeper/Program.cs
+++ vteCore.jbKeeper/Program.cs
@@ -1,4 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+using Serilog;
+using vteCore.jbKeeper;
+
 Console.WriteLine("Hello, World!");
 
 Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{environment}.json", true, true)
+    .AddEnvironmentVariables();
+var configurationRoot = configuration.Build();
+
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configurationRoot).CreateBootstrapLogger();
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "Service Core Job Keeper";
+});
+
+builder.Services.AddSerilog();
+//using targetframework windows to remove the compiler warning
+LoggerProviderOptions.RegisterProviderOptions<
+    EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+builder.Services.AddHostedService<JobKeeper>();
+
+IHost host = builder.Build();
+
+host.Run();
